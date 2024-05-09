124c6b24417b3ec705a45591a4bae388c88070b5
Zentifyyy <109432346+Zentifyyy@users.noreply.github.com>
2024-05-09 02:51:46 +0100

    palindrome-number = done

---
+++ 9.palindrome-number.cs
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=9 lang=csharp
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+public class Solution {
+    public bool IsPalindrome(int x) {
+        var xString = x.ToString();
+
+        if(xString.Length == 1){return true;}
+
+        bool result = true;
+        int inverseI = xString.Length;
+        
+        for(int i = 0; i < xString.Length / 2; i++){
+            
+            if(xString[i] != xString[inverseI - i - 1]){ return false;}
+        }
+
+        return true;
+    }
+}
+// @lc code=end
+
